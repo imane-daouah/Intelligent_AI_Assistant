deb9de9f4c47c0be98c1749c363a87699976139a
imane-daouah <imane.daouah@gmail.com>
2025-02-10 18:16:45 +0100

    commit

--- ivn/gui.py
+++ ivn/gui.py
@@ -29,34 +29,39 @@ def delete_text():
 
 
 root = Tk()
-root.geometry("550x675")
+#root.geometry("550x675")
+root.geometry("550x700")
+
 root.title("AI Assistant")
 root.resizable(False,False)
-root.config(bg="#6F8FAF")
+root.config(bg="#BEBEBE")
 
   
 
 
 # Main Frame
 Main_frame = LabelFrame(root , padx=100 ,  pady=7 , borderwidth=3 ,  relief="raised")
-Main_frame.config(bg="#6F8FAF")
+Main_frame.config(bg="#D3D3D3")
 Main_frame.grid(row = 0 ,  column= 1 ,  padx= 55 ,  pady =  10)
 
+
 # Text Lable 
-Text_lable = Label(Main_frame, text = "AI Assistant" , font=("comic Sans ms" ,  14 , "bold" ) , bg = "#356696")
+Text_lable = Label(Main_frame, text = "AI Assistant" , font=("comic Sans ms" ,  14 , "bold" ) , bg = "#FFFFFF")
 Text_lable.grid(row=0 ,  column=0 , padx=20 , pady= 10)
 
 
-# Image 
-Display_Image = ImageTk.PhotoImage(Image.open("image/assistant.png"))
-Image_Lable = Label(Main_frame, image= Display_Image)
-Image_Lable.grid(row = 1 ,  column=0 , pady=20)
 
-
+# Charger et redimensionner l'image
+original_image = Image.open("image/assistant.png")
+resized_image = original_image.resize((240, 240))  # Modifier la taille (largeur, hauteur)
+Display_Image = ImageTk.PhotoImage(resized_image)
+Image_Lable = Label(Main_frame, image=Display_Image)
+Image_Lable.grid(row=1, column=0, pady=20)
+Image_Lable.image = Display_Image  
 
 # Add a text widget
 
-text=Text(root , font= ('Courier 10 bold') , bg = "#356696")
+text=Text(root , font= ('Courier 10 bold') , bg = "#FFFFFF")
 text.grid(row = 2,  column= 0)
 text.place(x= 100, y= 375, width= 375, height= 100) 
 
@@ -70,14 +75,14 @@ entry1.place(x=100 , y = 500 , width= 350, height= 30)
 
 
 # Add a text button1
-button1 =  Button(root,  text="ASK" , bg="#356696" , pady=16 ,  padx=40,  borderwidth=3 , relief=SOLID ,  command=ask)
+button1 =  Button(root,  text="ASK" , bg="#FFFFFF" , pady=16 ,  padx=40,  borderwidth=3 , relief=SOLID ,  command=ask)
 button1.place(x= 70, y= 575)
 
 # Add a text button2
-button2 =  Button(root,  text="Send" , bg="#356696" , pady=16 ,  padx=40,  borderwidth=3 , relief=SOLID ,  command=User_send)
+button2 =  Button(root,  text="Send" , bg="#FFFFFF" , pady=16 ,  padx=40,  borderwidth=3 , relief=SOLID ,  command=User_send)
 button2.place(x= 400, y= 575)
 
 # Add a text button3
-button3 = Button(root, text="Delete", bg="#356696" , pady=16 ,  padx=40,  borderwidth=3 , relief=SOLID ,command=delete_text)
+button3 = Button(root, text="Delete", bg="#FFFFFF" , pady=16 ,  padx=40,  borderwidth=3 , relief=SOLID ,command=delete_text)
 button3.place(x= 225, y= 575)
 root.mainloop()
